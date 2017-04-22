09a1086c06956d2a3ca62e1683fccba65f1cd6c5
Samuel Betrisey <me@sam.ninja>
2017-04-22 21:41:07 +0200

    Add original solution for problem B

--- src/main/scala/ninja/sam/codejam/R1B/Bv2.scala
+++ src/main/scala/ninja/sam/codejam/R1B/Bv2.scala
@@ -6,7 +6,7 @@ import ninja.sam.codejam.Solver
 
 import scala.collection.mutable.LinkedHashMap
 
-object B extends Solver(inputFile = "B-small-attempt0.in") {
+object Bv2 extends Solver(inputFile = "B-large.in") {
   override type Input = Map[Char, Int]
   override type Output = Option[String]
 
@@ -21,7 +21,46 @@ object B extends Solver(inputFile = "B-small-attempt0.in") {
   override def apply(input: Input): Output = {
     var poneys = input.flatMap{case (c, n) => c.toString * n}.toList
 
-    val sorted = poneys.sortBy(c => poneys.count(_ == c))
+    def countSameHair(c: Char) : Int = {
+      if (c == 'R'||c == 'Y'||c == 'B') {
+        return poneys.count(_ == c)
+      } else if (c == 'O') {
+        return poneys.count(x => x == 'R'||x == 'Y'||x == 'O')
+      } else if (c == 'G') {
+        return poneys.count(x => x == 'Y'||x == 'B'||x == 'G')
+      } else if (c == 'V') {
+        return poneys.count(x => x == 'R'||x == 'B'||x == 'V')
+      }
+      return 0
+    }
+
+    def sameHair (c1: Char, c2: Char): Boolean = {
+      if (c1 == c2) return true
+
+      if (c1 == 'O') {
+        return c2 == 'R'||c2 == 'Y'
+      }
+      if (c1 == 'G') {
+        return c2 == 'Y'||c2 == 'B'
+      }
+      if (c1 == 'V') {
+        return c2 == 'R'||c2 == 'B'
+      }
+
+      if (c2 == 'O') {
+        return c1 == 'R'||c1 == 'Y'
+      }
+      if (c2 == 'G') {
+        return c1 == 'Y'||c1 == 'B'
+      }
+      if (c2 == 'V') {
+        return c1 == 'R'||c1 == 'B'
+      }
+
+      return false
+    }
+
+    val sorted = poneys.sortBy(countSameHair)
 
     val newArray = new Array[Char](sorted.length)
 
@@ -37,10 +76,10 @@ object B extends Solver(inputFile = "B-small-attempt0.in") {
       }
     }
 
-    var ok = (newArray.head != newArray.last||newArray.length == 1)
+    var ok = (!sameHair(newArray.head, newArray.last)||newArray.length == 1)
     var i = 0
     while (ok&&i < poneys.length - 1) {
-      ok = newArray(i) != newArray(i+1)
+      ok = !sameHair(newArray(i), newArray(i+1))
       i += 1
     }
 
